bc5069f0b40c10f171f8482c222e7786b2fb3f9b
Victor Rodrigues <victorrsilva17@gmail.com>
2022-07-13 23:21:25 -0300

    Adicionando atributos

--- estacionamento/src/Estacionamento.java
+++ estacionamento/src/Estacionamento.java
@@ -2,6 +2,20 @@
 public class Estacionamento {
 
 	public static void main(String[] args) {
-		System.out.println("Olá Mundo!");
+		float valorFracao;
+		float valorHoraCheia;
+		float valorDiariaDiurna;
+		float valorDiariaNoturna;
+		float valorMensalidade;
+		float valorEvento;
+		String inicioFuncionamento;
+		String fimFuncionamento;
+		int capacidade;
+		float retornoContratante;
+		
+	}
+	
+	void cadastratAcesso(String placa, String horaEntrada, String horaSaida) {
+		
 	}
 }
