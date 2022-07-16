f1b4766f02b3a9d00577e6a11fbd96087e9d56bf
Victor Rodrigues <victorrsilva17@gmail.com>
2022-07-16 20:15:52 -0300

    Classe Acesso com calculos de permanencia
    Co-Authored-By: Francisco Heronildo <heronildo.2010@hotmail.com>

---
+++ estacionamento/src/Estacionamento/Acesso.java
@@ -0,0 +1,54 @@
+package Estacionamento;
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+
+import static java.time.temporal.ChronoUnit.MINUTES;
+
+public class Acesso {
+	String placa;
+	String horaEntrada;
+	String horaSaida;
+	Estacionamento estacionamento;
+	String tipoAcesso;
+	
+	public Acesso(String placa, String horaEntrada, String horaSaida, Estacionamento estacionamento, String tipoAcesso) {
+		super();
+		this.placa = placa;
+		this.horaEntrada = horaEntrada;
+		this.horaSaida = horaSaida;
+		this.estacionamento = estacionamento;
+		this.tipoAcesso = tipoAcesso;
+	}
+	
+	public float calculaAcesso() {
+		if (tipoAcesso == "evento") {
+			return estacionamento.valorEvento;
+		}
+		else if(tipoAcesso == "mensalista") {
+			return estacionamento.valorMensalidade;
+		}
+		else {
+			DateTimeFormatter formatTime = DateTimeFormatter.ofPattern("dd-MM-yyyy HH:mm:ss");
+			
+			LocalDateTime horaEntradaFormat = LocalDateTime.parse(this.horaEntrada, formatTime);
+			LocalDateTime horaSaidaFormat = LocalDateTime.parse(this.horaSaida, formatTime);
+			LocalDateTime inicioHoraNoturnaFormat = LocalDateTime.parse(this.estacionamento.inicioHoraNoturna, formatTime);
+			
+			float tempoPermanencia = horaEntradaFormat.until(horaSaidaFormat, MINUTES);
+			int qtdFracoes = (int) Math.ceil(tempoPermanencia/15f);
+			
+			if(horaEntradaFormat.until(inicioHoraNoturnaFormat, MINUTES) <= 0) {
+				return this.estacionamento.valorDiariaNoturna * this.estacionamento.valorDiariaDiurna;
+			}
+			else if (qtdFracoes < 4) {
+				return this.estacionamento.valorFracao * qtdFracoes;
+			}
+			else if (qtdFracoes >= 4 && qtdFracoes <= 36) {
+				return (this.estacionamento.valorFracao * qtdFracoes) * (1 - this.estacionamento.valorHoraCheia);
+			}
+			else {
+				return this.estacionamento.valorDiariaDiurna;
+			}
+		}
+	}
+}
