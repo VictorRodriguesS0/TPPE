7b9af199c26313c512577efa84a73e187f4a4383
Francisco Heronildo <heronildo.2010@hotmail.com>
2022-07-18 20:51:55 -0300

    TRIANGULAÇÃO: Calculo do valor apurado pela contratante
    Co-Authored-By: Victor Rodrigues <victorrsilva17@gmail.com>

--- estacionamento/src/Testes/ValorContratanteTeste.java
+++ estacionamento/src/Testes/ValorContratanteTeste.java
@@ -15,6 +15,7 @@ public class ValorContratanteTeste {
 	Estacionamento estacionamento, estacionamento2, estacionamento3;
 	ArrayList<Acesso> acessos = new ArrayList();
 	ArrayList<Acesso> acessos2 = new ArrayList();
+	ArrayList<Acesso> acessos3 = new ArrayList();
 	
 	
 
@@ -32,7 +33,7 @@ public class ValorContratanteTeste {
 		
 		
 		estacionamento2 = new Estacionamento("Estacionamento2", 30.0f, 0.15f, 120.0f, 0.45f, "16-07-2022 19:00:00",
-				"17-07-2022 08:00:00", 600f, 50f, 0.50f);
+				"17-07-2022 08:00:00", 455f, 50f, 0.50f);
 		
 		Acesso acessoEvento2 = new Acesso("HI139", "16-07-2022 09:00:00", "17-07-2022 10:00:00", estacionamento2, "evento");
 		Acesso acessoDiariaDiurna2 = new Acesso("HI138", "16-07-2022 08:00:00", "17-07-2022 18:00:00", estacionamento2, null);
@@ -40,6 +41,16 @@ public class ValorContratanteTeste {
 		acessos2.add(acessoEvento2);
 		acessos2.add(acessoDiariaDiurna2);
 		acessos2.add(acessoFracoes2);
+		
+		estacionamento3 = new Estacionamento("Estacionamento3", 10.0f, 0.0f, 50.0f, 0.40f, "16-07-2022 20:00:00",
+				"17-07-2022 08:00:00", 350f, 40f, 0.70f);
+		
+		Acesso acessoEvento3 = new Acesso("HI139", "16-07-2022 09:00:00", "16-07-2022 10:00:00", estacionamento3, "evento");
+		Acesso acessoDiariaDiurna3 = new Acesso("HI138", "16-07-2022 08:00:00", "16-07-2022 18:00:00", estacionamento3, null);
+		Acesso acessoFracoes3 = new Acesso("HI137", "16-07-2022 08:00:00", "16-07-2022 08:45:00", estacionamento3, null);
+		acessos3.add(acessoEvento3);
+		acessos3.add(acessoDiariaDiurna3);
+		acessos3.add(acessoFracoes3);
 		
 	}
 
@@ -57,4 +68,13 @@ public class ValorContratanteTeste {
 
 	}
 
+	
+	@Test
+	public void calculaValorContratanteTriplicada() {
+		 float calcula = estacionamento3.calcularContratante(acessos3);
+		 System.out.println(calcula);
+		 assertEquals(84, calcula, 0f);
+
+	}
+	
 }
