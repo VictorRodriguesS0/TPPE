0aa67eda4ab1b6f116d4503f7454e011c9cb5fe6
Victor Rodrigues <victorrsilva17@gmail.com>
2022-07-18 20:08:08 -0300

    Adicionando construtor a classe estacionamento
    Co-Authored-By: Francisco Heronildo <heronildo.2010@hotmail.com>

--- estacionamento/src/Estacionamento/Estacionamento.java
+++ estacionamento/src/Estacionamento/Estacionamento.java
@@ -19,7 +19,6 @@ public class Estacionamento {
 	int numeroAcessos;
 	float valorApurado;
 	String nomeEstacionamento;
-	
 
 	public Estacionamento(float valorFracao, float valorHoraCheia, float valorDiariaDiurna, float valorDiariaNoturna,
 			float valorMensalidade, float valorEvento, String inicioFuncionamento, String fimFuncionamento,
@@ -74,13 +73,29 @@ public class Estacionamento {
 		this.nomeEstacionamento = nomeEstacionamento;
 	}
 
-
 	public Estacionamento(float valorFracao, float valorHoraCheia, String nomeEstacionamento) {
 		super();
 		this.valorFracao = valorFracao;
 		this.valorHoraCheia = valorHoraCheia;
 		this.nomeEstacionamento = nomeEstacionamento;
 	}
+
+	public Estacionamento(String nomeEstacionamento, float valorFracao, float valorHoraCheia, float valorDiariaDiurna,
+			float valorDiariaNoturna, String inicioHoraNoturna, String fimHoraNoturna, float valorMensalidade,
+			float valorEvento, float retornoContratante) {
+		super();
+		this.valorFracao = valorFracao;
+		this.valorHoraCheia = valorHoraCheia;
+		this.valorDiariaDiurna = valorDiariaDiurna;
+		this.valorDiariaNoturna = valorDiariaNoturna;
+		this.valorMensalidade = valorMensalidade;
+		this.valorEvento = valorEvento;
+		this.inicioHoraNoturna = inicioHoraNoturna;
+		this.fimHoraNoturna = fimHoraNoturna;
+		this.retornoContratante = retornoContratante;
+		this.nomeEstacionamento = nomeEstacionamento;
+	}
+
 	public float calcularContratante(ArrayList<Acesso> listaAcessos) {
 		float total = 0;
 		for (Acesso acesso : listaAcessos) {
