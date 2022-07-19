a24108f7bd9e9a7c9e2956f6aa8987ec0dd82690
Victor Rodrigues <victorrsilva17@gmail.com>
2022-07-18 22:51:13 -0300

    Suite de Testes Funcionais
    Co-Authored-By: Francisco Heronildo <heronildo.2010@hotmail.com>

---
+++ estacionamento/src/Testes/TestesSuite.java
@@ -0,0 +1,11 @@
+package Testes;
+
+import org.junit.runner.RunWith;
+import org.junit.runners.Suite;
+import org.junit.runners.Suite.SuiteClasses;
+
+@RunWith(Suite.class)
+@SuiteClasses({ AcessoHoraCheiaTeste.class, AcessoNoturnoTeste.class, ValorContratanteTeste.class })
+public class TestesSuite {
+
+}
