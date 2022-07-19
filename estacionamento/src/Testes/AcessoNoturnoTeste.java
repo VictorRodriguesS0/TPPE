008a7e43de6ea2ed430272a11a8b7543fde48627
Francisco Heronildo <heronildo.2010@hotmail.com>
2022-07-18 21:44:24 -0300

    TESTE PARAMETRIZADO:  Cálculo do valor de acesso para acesso noturno
    Co-Authored-By: Victor Rodrigues <victorrsilva17@gmail.com>

--- estacionamento/src/Testes/AcessoNoturnoTeste.java
+++ estacionamento/src/Testes/AcessoNoturnoTeste.java
@@ -2,50 +2,58 @@ package Testes;
 
 import static org.junit.Assert.assertEquals;
 
+import java.util.Arrays;
+
 import org.junit.Before;
 import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.Parameterized;
+import org.junit.runners.Parameterized.Parameters;
 
 import Estacionamento.Acesso;
 import Estacionamento.Estacionamento;
 
+@RunWith(Parameterized.class)
 public class AcessoNoturnoTeste {
 
 	Estacionamento estacionamento, estacionamento2, estacionamento3;
-	Acesso acesso, acesso2, acesso3;
+	Acesso acesso, acesso2, acesso3, acessoteste;
 	private String tipoAcesso;
+	private String placa;
+	private String horaEntrada;
+	private String horaSaida;
+	float valorTotal;
 
 	@Before
 	public void setup() {
 		estacionamento = new Estacionamento("Estacionamento1", 20.0f, 0.10f, 70.0f, 0.30f, "16-07-2022 21:00:00",
 				"17-07-2022 07:00:00");
-		estacionamento2 = new Estacionamento("Estacionamento2", 30.0f, 0.15f, 120.0f, 0.45f, "16-07-2022 19:00:00",
-				"16-07-2022 08:00:00");
-		estacionamento3 = new Estacionamento("Estacionamento2", 10.0f, 0.0f, 50.0f, 0.40f, "16-07-2022 20:00:00",
-				"17-07-2022 08:00:00");
-		acesso = new Acesso("HI139", "16-07-2022 21:36:00", "17-07-2022 06:12:00", estacionamento, tipoAcesso);
-		acesso2 = new Acesso("HI138", "16-07-2022 21:00:00", "16-07-2022 22:00:00", estacionamento2, tipoAcesso);
-		acesso3 = new Acesso("HI138", "16-07-2022 21:00:00", "16-07-2022 21:15:00", estacionamento3, tipoAcesso);
+		acessoteste = new Acesso(placa, horaEntrada, horaSaida, estacionamento, tipoAcesso);
 
+	}
+
+	public AcessoNoturnoTeste(String placa, String horaEntrada, String horaSaida, float valorTotal) {
+		this.placa = placa;
+		this.horaEntrada = horaEntrada;
+		this.horaSaida = horaSaida;
+		this.valorTotal = valorTotal;
+	}
+
+	@Parameters
+	public static Iterable<Object[]> getParameters() {
+		Object[][] respostas = new Object[][] { 
+			{ "HI139", "16-07-2022 21:36:00", "17-07-2022 06:12:00", 21f },
+			{ "HI138", "16-07-2022 21:00:00", "16-07-2022 22:00:00", 21f },
+			{ "HI137", "16-07-2022 22:00:00", "16-07-2022 23:59:00", 21f }, 
+			};
+
+		return Arrays.asList(respostas);
 	}
 
 	@Test
 	public void calculaValorAcessoNoturno() {
-		float calcula = acesso.calculaAcesso();
-		assertEquals(21, calcula, 0f);
-
-	}
-
-	@Test
-	public void calculaValorAcessoNoturno2() {
-		float calcula2 = acesso2.calculaAcesso();
-		assertEquals(54, calcula2, 0f);
-
-	}
-	
-	@Test
-	public void calculaValorAcessoNoturno3() {
-		float calcula3 = acesso3.calculaAcesso();
-		assertEquals(20, calcula3, 0f);
+		float calculo = acessoteste.calculaAcesso();
+		assertEquals(valorTotal, calculo, 0f);
 
 	}
 
